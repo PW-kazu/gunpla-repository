b4d8bfa6a337076bc40e141fb1f7861713734b00
PW kazu <komura_jutasrou@icloud.com>
2022-12-09 17:10:10 +0900

    corrected

--- app/Models/Modeller.php
+++ app/Models/Modeller.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Modeller extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name','email'];
+    
+    public function getDetail()
+    {
+        $txt = 'ID:'.$this->id . '' . $this->name . $this->email . $this->password . $this->zip . $this->prefecture . $this->address . $this->tel;
+    }
+
+    public function modellerss()
+    {
+        return $this->hasMany('App\Models\Modeller');
+    }
+
 }
